05a114cada05a094c288ade4cd98c37bfaa26e5b
assilva@apoioprodesp.sp.gov.br <assilva@apoioprodesp.sp.gov.br>
2024-11-10 21:42:45 -0300

    primeiro commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,141 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from 'react';
+import { GoogleMap, LoadScript, Marker, DirectionsRenderer } from '@react-google-maps/api';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Loader2 } from "lucide-react";
+import { toast } from '@/hooks/use-toast';
+
+export type selectLocationType = {
+  name: string | null;
+  id: number;
+  position: [
+    lat: string | google.maps.LatLng | google.maps.Place | google.maps.LatLngLiteral,
+    lng: string | google.maps.LatLng | google.maps.Place | google.maps.LatLngLiteral,
+  ]
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [userAddress, setUserAddress] = useState('');
+  const [directions, setDirections] = useState(null);
+  const [selectedLocation, setSelectedLocation] = useState<selectLocationType>();
+  const [loading, setLoading] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const locations = [
+    { id: 1, name: 'Loja 1 - Teste', position: { lat: -23.52450081012362, lng: -46.64245413280253 } },
+    { id: 2, name: 'Loja 2 - Teste', position: { lat: -23.543106513921813, lng: -46.641429318655256 } },
+    { id: 3, name: 'Loja 3 - Teste', position: { lat: -23.537101263272856, lng: -46.57552208981917 } },
+    // Adicione mais localizações aqui
+  ];
+
+  const handleUserAddressChange = (e) => setUserAddress(e.target.value);
+
+  const traceRoute = () => {
+
+    //if (!userAddress || !selectedLocation) return;
+    if (!selectedLocation?.name){
+      toast({
+        title: "Alerta",
+        variant: "destructive",
+        description: "A loja não foi selecionada. ",
+      })
+      return
+    }
+
+    if (!userAddress){
+      toast({
+        title: "Alerta",
+        variant: "destructive",
+        description: "Não foi possível traçar a rota. Verifique o endereço inserido.",
+      })
+      return
+    }
+
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    // Inicializar o DirectionsService para traçar a rota
+    const service = new window.google.maps.DirectionsService();
+    service.route(
+      {
+        origin: userAddress,
+        destination: selectedLocation.position,
+        travelMode: window.google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (status === window.google.maps.DirectionsStatus.OK) {
+          setDirections(result);
+        } else {
+          alert('Não foi possível traçar a rota. Verifique o endereço inserido.');
+        }
+      }
+    );
+  };
+
+  return (
+    <div>
+      <LoadScript googleMapsApiKey="AIzaSyBmXL8wzrL5Y_dBfeco202Np8XpjcSKZrY">
+      <h1>Minhas Lojas</h1>
+      <GoogleMap
+        mapContainerStyle={{ width: '100%', height: '500px' }}
+        center={{ lat: -23.55052, lng: -46.633308 }}
+        zoom={10}
+      >
+        {locations.map((loc) => (
+          <Marker
+            key={loc.id}
+            position={loc.position}
+            onClick={() => 
+              setSelectedLocation(loc)
+            }
+            title={loc.name}
+          />
+        ))}
+
+        {directions && <DirectionsRenderer directions={directions} />}
+      </GoogleMap>
+
+      <div style={{ marginTop: '20px' }}>
+        <div className="grid w-full max-w-lg items-center gap-1.5">
+          <Label htmlFor="ljSelecionada">Loja Selecionada:</Label>
+
+          {selectedLocation?.name &&
+            <Input
+              id="ljSelecionada"
+              type="text"
+              placeholder="Loja selecionada"
+              value={selectedLocation?.name}
+              disabled="disabled"
+              />
+          }
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      </div>
+
+      <div style={{ marginTop: '20px' }}>
+
+        <div className="grid w-full max-w-lg items-center gap-1.5">
+          <Label htmlFor="endereco">Endereço:</Label>
+          <Input
+            id="endereco"
+            type="text"
+            placeholder="Digite seu endereço"
+            value={userAddress}
+            onChange={handleUserAddressChange}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        {loading && <Button disabled>Traçar Rota<Loader2 className="animate-spin" /></Button>}
+        {!loading && <Button onClick={traceRoute} >Traçar Rota</Button>}
+        
+        </div>
+
+        {/* {selectedLocation && <p>Destino: {selectedLocation.name}</p>} */}
+      </div>
+    </LoadScript>
+  </div>
   );
 }
